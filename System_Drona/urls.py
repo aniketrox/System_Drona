347dcef771e278fd25d74d2ff05684228b9b3266
rohitpd18 <91771522+rohitpd18@users.noreply.github.com>
2022-08-22 23:17:48 +0530

    reg page

--- System_Drona/urls.py
+++ System_Drona/urls.py
@@ -23,8 +23,10 @@ urlpatterns = [
     path('teacher/', include('teacher.urls')),
     path('contactus/', views.contactus),
     path('about', views.about),
+    # path('registration', views.registration),
     path('singin', views.singin),
-    path('singin/form', views.singinform),
+    path('singin/form', views.registration),
+    path('singin/form/takephoto', views.takephoto),
     path('', views.index, name='index'),
     path("__reload__/", include("django_browser_reload.urls")),
 ] 
--- System_Drona/views.py
+++ System_Drona/views.py
@@ -5,28 +5,42 @@ def index(request):
 
 def contactus(request):
     param= {
-        "user": 'EMD',
+        "user": 'System Drona',
         'currPage': 'Contact Us'
     }
     return render(request, 'contactus.html', param)
 
 def about(request):
     param= {
-        "user": 'EMD',
+        "user": 'System Drona',
         'currPage': 'About'
     }
     return render(request, 'about.html', param)
 
+def registration(request):
+    param= {
+        "user": 'System Drona',
+        'currPage': 'Registration'
+    }
+    return render(request, 'reg.html', param)
+
+def takephoto(request):
+    param= {
+        "user": 'System Drona',
+        'currPage': 'Registration/ Takephoto'
+    }
+    return render(request, 'takephoto.html', param)
+
 def singin(request):
     param= {
-        "user": 'EMD',
+        "user": 'System Drona',
         'currPage': 'Sing In'
     }
     return render(request, 'singin.html', param)
 
 def singinform(request):
     param= {
-        "user": 'EMD',
+        "user": 'System Drona',
         'currPage': 'Sing In-form'
     }
     return render(request, 'singinform.html', param)
